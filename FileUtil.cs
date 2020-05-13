9a6adf979476f72b9e23be4c06af9b5706f24e4c
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-05-13 22:09:19 +0200

    Initial commit

---
+++ FileUtil.cs
@@ -0,0 +1,12 @@
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+using System;
+
+public static class FileUtil
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+            => js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+    }
